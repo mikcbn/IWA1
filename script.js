ec8b9cd78184f60d9a121e5b0d3307694257597e
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-07-31 15:18:28 +0200

    Added value to button
    Added a value so that each time either button is clicked the number changes by 1 according to the button clicked

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
-const subtractHandler
-const addHandler
 
-subtract.addEventListener('click', )
+const subtractHandler = () => {
+    const newValue = parseInt(number.value) -1
+    number.value = newValue
 
-add.addEventListener('click',)
+}
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) +1
+    number.value = newValue
+
+}
+
+subtract.addEventListener('click', subtractHandler)
+
+add.addEventListener('click', addHandler)

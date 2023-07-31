ebc99aa25c9610b47c74370da9b14c03c96a21f8
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-07-31 14:30:41 +0200

    Created JavaScript folder

---
+++ script.js
@@ -0,0 +1,10 @@
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+const subtractHandler
+const addHandler
+
+subtract.addEventListener('click', )
+
+add.addEventListener('click',)
